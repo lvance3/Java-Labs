04f9bc8d6a1033a925541cadb412d118b209867a
lvance3 <142830671+lvance3@users.noreply.github.com>
2023-09-06 14:07:03 -0400

    Update Lab1.java
    added code for codon generation with weighted values and 'AAA' count

--- src/lab1/Lab1.java
+++ src/lab1/Lab1.java
@@ -4,12 +4,51 @@ import java.util.Random;
 
 public class Lab1
 {
-	public static void main(String[] args)
+    public static void main(String[] args)
 	{
-		Random random = new Random();
-		
-		// generate two random numbers between 0 and 3...
-		System.out.println(random.nextInt(4));
-		System.out.println(random.nextInt(4));
-	}
+        Random random = new Random();
+
+        String CODON = "";
+        int ID = 0;
+        int AAA = 0;
+
+        for (int i=0; i<1000; i++)
+        {
+            for (int j=1; j<=3; j++)
+            {
+                float f = random.nextFloat();
+
+                if (f > 0.0 && f <= 0.12) //adjusted rate
+                //if (f > 0.0 && f <= 0.25) //unadjusted rate
+                      
+                    CODON = CODON + "A";
+            
+                if (f > 0.12 && f <= 0.5) //adjusted rate
+                //if (f > 0.25 && f <= 0.5) //unadjusted rate
+                
+                    CODON = CODON + "C";
+                
+                if (f > 0.5 && f <= 0.89) //adjusted rate
+                //if (f > 0.5 && f <= 0.75) //unadjusted rate
+                
+                    CODON = CODON + "G";
+                
+                if (f > 0.89 && f <= 1) //adjusted rate
+                //if (f > 0.75 && f <= 1) //unadjusted rate
+                
+                    CODON = CODON + "T";
+                if (CODON.length() == 3)
+                {
+                    ID++;
+                    System.out.println(ID + ": " + CODON);
+
+                    if (CODON.equals("AAA"))
+                        AAA++;
+
+                    CODON = "";
+                }
+            }
+        }
+        System.out.println("AAA Count: " + AAA);
+    }
 }
